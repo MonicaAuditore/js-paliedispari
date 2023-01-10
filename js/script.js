7d0689d3244d1b99f44c63825002eb35b8c33eeb
MonicaAuditore <mauditore@live.it>
2023-01-10 14:39:33 +0100

    add_parte due

--- js/script.js
+++ js/script.js
@@ -14,19 +14,21 @@ Dichiariamo chi ha vinto.
 /*
 Istruzioni:
 1. Con prompt, chiedere all'utente di inserire una parola;                        -- ok
-2. Creare una funzione per capire se la parola inserita è palindroma;
+2. Creare una funzione per capire se la parola inserita è palindroma;             -- ok
 
-3. Con prompt, chiedere all'utente di scegliere pari o dispari;
-4. Con prompt, chiedere all'utente di inserire un numero tra 1 e 5;
+3. Con prompt, chiedere all'utente di scegliere pari o dispari;                   -- ok
+4. Con prompt, chiedere all'utente di inserire un numero tra 1 e 5;               -- ok
 5. Creare una funzione per generare un numero random da 1 a 5 per il computer;
 6. Sommare i due numeri;
 7. Stabilire se la somma dei numeri è pari o dispari;
 8. Dichiarare il vincitore.
  */
 
+// Chiedo all'utente di inserire una parola
 let parolaUtente = prompt("Inserisci una parola");
 console.log("parola inserita utente", parolaUtente);
 
+// Creo una funzione per capire se la parola inserita è palindroma
 function palindroma() {
   let parolaArray = parolaUtente.split("");
   console.log("parola nell array", parolaArray);
@@ -49,4 +51,20 @@ function palindroma() {
 }
 
 let risultato = palindroma(parolaUtente);
-console.log("Risultato", risultato);
+console.log("La parola è palindroma?", risultato);
+
+// Chiedo all'utente di scegliere pari o dispari
+const pariOdispari = prompt("Scegli pari o dispari");
+console.log("Pari o dispari?", pariOdispari);
+
+// Chiedo all'utente di inserire un numero da 1 a 5
+const utenteNumero = prompt("Scegli un numero da 1 a 5");
+console.log("Scelta numero utente", utenteNumero);
+
+// Creo una funzione per generare un numero random da 1 a 5
+function numeroRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+let numeroGenerato = numeroRandom(1, 5);
+console.log("Scelta numero computer", numeroGenerato);
