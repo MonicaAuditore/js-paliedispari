d5d02361b3cdb8ba0c909629b0809c37e6f1fc6f
MonicaAuditore <mauditore@live.it>
2023-01-10 13:20:57 +0100

    init_inizializzazione progetto

--- js/script.js
+++ js/script.js
@@ -1,8 +1,5 @@
-# js-paliedispari
-
-Esercizio di oggi: Pali e dispari
-nome repo: js-paliedispari
-Palidroma
+/*
+ Palidroma
 Chiedere all'utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma
 Pari e Dispari
@@ -11,7 +8,9 @@ Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzion
 Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
-Consigli del giorno
+ */
 
-1. Scriviamo sempre in italiano i passaggi che vogliamo fare
-2. Scriviamo sempre solo un pezzetto di codice alla volta, se funziona allora andiamo avanti.
+/*
+Istruzioni:
+1.
+ */
